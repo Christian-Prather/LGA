acdec20c9eb334c6c39017a934d108b6cfb3c4dd
Clara Larson <calarson@mymail.mines.edu>
2021-02-25 21:19:23 -0700

    Removed my vscode stuff, sorry about that

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/clara/OneDrive/2021 Spring Mines/Compilers/LGA
-# Build directory: /mnt/c/Users/clara/OneDrive/2021 Spring Mines/Compilers/LGA/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

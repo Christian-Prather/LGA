5a65890cdcb6e68d97293194e887f74c10e3ac39
Christian Prather <christian@luci.com>
2021-03-21 23:29:16 -0600

    Reduce added but segfaulting

--- include/actionTable.h
+++ include/actionTable.h
@@ -51,7 +51,7 @@ vector<int> searchSets(vector<ItemSet> itemSets, Item searchItem, set<string> he
             for (auto character : headerRow)
             {
 
-                if (item.progressMarkerIndex == searchItem.progressMarkerIndex + 1 && item.rule[item.progressMarkerIndex] == character)
+                if (item.progressMarkerIndex == searchItem.progressMarkerIndex + 1 && item.rule.RHS[item.progressMarkerIndex] == character)
                 {
                     // Potential
                     if (sameRule(item.rule, searchItem.rule))
@@ -66,6 +66,21 @@ vector<int> searchSets(vector<ItemSet> itemSets, Item searchItem, set<string> he
     return matches;
 }
 
+// vector<int> reduce(vector<ItemSet> ItemSets, Item )
+
+int grammarRule(vector<string> rhs, CFG cfg)
+{
+    for (int i = 0; i < cfg.rules.size(); i++)
+    {
+        auto rule = cfg.rules[i];
+        if (rule.RHS == rhs)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
 ActionTable buildActionTable(vector<ItemSet> itemSets, CFG cfg)
 {
     // Combine sets
@@ -73,32 +88,70 @@ ActionTable buildActionTable(vector<ItemSet> itemSets, CFG cfg)
     set<string> header(cfg.terminals);
     header.insert(cfg.nonTerminals.begin(), cfg.nonTerminals.end());
 
-    for (auto set : itemSets)
+    for (auto itemSet : itemSets)
     {
-        for (auto rule : set.itemSet)
+        for (auto rule : itemSet.itemSet)
         {
             // S-> fa*
-            vector<int> matches = searchSets(itemSets, rule, header);
+            // Intialixe Row
+            SlrRow row;
+            for (int i = 0; i < header.size(); i++)
             {
-                if (matches.size() > 0)
-                {
-                    ActionGoTO entry;
-                    entry.action = Shift;
-                    entry.goTo = matches[0]; // Doesnt handle conflic
+                ActionGoTO entity;
+                entity.action = None;
+                entity.goTo = -1;
+                row.entries.push_back(entity);
+            }
 
-                    SlrRow row;
-                    row.entries.push_back(entry);
-                    actionTable.rows.push_back(row);
-                }
-                else
-                {
-                    ActionGoTO entry;
-                    entry.action = None;
-                    entry.goTo = -1; // Doesnt handle conflic
+            vector<int> matches = searchSets(itemSets, rule, header);
 
-                    SlrRow row;
-                    row.entries.push_back(entry);
-                    actionTable.rows.push_back(row);
+            if (matches.size() > 0)
+            {
+                ActionGoTO entry;
+                entry.action = Shift;
+                entry.goTo = matches[0]; // Doesnt handle conflic
+
+                // SlrRow row;
+                row.entries.push_back(entry);
+                actionTable.rows.push_back(row);
+                continue;
+            }
+
+            if (rule.progressMarkerIndex == rule.rule.RHS.size() - 1)
+            {
+                int grammar = grammarRule(rule.rule.RHS, cfg);
+                if (grammar != -1)
+                {
+                    set<string> followT;
+                    FollowResult follow = followSet(rule.rule.LHS, followT, cfg);
+                    set<string>::iterator terminalsIt = cfg.terminals.begin();
+                    for (int i = 0; i < cfg.terminals.size(); i++)
+                    {
+                        for (string f : follow.F)
+                        {
+                            if (f == *terminalsIt)
+                            {
+                                row.entries[i].action = Reduce;
+                                row.entries[i].goTo = grammar;
+                                // row.[i] = entry;
+                            }
+                        }
+                        terminalsIt++;
+                    }
+                    set<string>::iterator nonTerminalsIt = cfg.terminals.begin();
+
+                    for (int i = 0; i < cfg.nonTerminals.size(); i++)
+                    {
+                        for (string f : follow.F)
+                        {
+                            if (f == *nonTerminalsIt)
+                            {
+                                row.entries[i].action = Reduce;
+                                row.entries[i].goTo = grammar;
+                            }
+                        }
+                        nonTerminalsIt++;
+                    }
                 }
             }
         }
